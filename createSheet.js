24f36773e8b5c0a42cdadc360ab96bb75a4e14e5
nowaytohell <moez.sadok@gmail.com>
2015-12-01 00:49:21 +0100

    Update createSheet.js

--- createSheet.js
+++ createSheet.js
@@ -149,6 +149,7 @@ $(document).on("click", "[id$='w_PC_cCoupon_lnkConferma']", function(){
 $(document).on("click", "[id$='w_PC_cCoupon_btnStampaSco']", function(){
     
 	var updatedData='empty';
+	console.log("set data in json file");
 	$.get("https://api.myjson.com/bins/2gmih", function(data, textStatus, jqXHR) {
 		var currentvalue=data.allsheet;
 		console.log(currentvalue);
